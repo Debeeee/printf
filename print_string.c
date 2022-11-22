6be8bad34eb7f392e72cc1729359375da35a03e8
Debeeee <denenche11@gmail.com>
2022-11-22 06:01:46 -0800

    update

--- print_string.c
+++ print_string.c
@@ -1,26 +1,7 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
-=======
- * print_char - returns the character
- * @buff_dest: buffer
- * @arg: list of arguments
- * @buff_count: index of buffer pointer
- * Return: buffer index
- */
-int print_char(char *buff_dest, va_list arg, int buff_count)
-{
-	char c = va_arg(arg, int);
-
-	buff_dest[buff_count] = c;
-
-	return (++buff_count);
-}
-
-/**
->>>>>>> 376bba663e0907b6a8e7886299124b0a9a1da23e
- * print_str - writes the string
+ * print_str -writes the string 
  * @buff_dest: character string
  * @arg: list of arguments
  * @buff_count: index f buffer pointer
@@ -49,29 +30,4 @@ int print_str(char *buff_dest, va_list arg, int buff_count)
 	}
 
 	return (buff_count);
-<<<<<<< HEAD
 }
-=======
-}
-
-#include "main.h"
-
-/**
- * print_perc - returns the character
- * @buff_dest: character
- * @arg: list of arguments
- * @buff_count: index of buffer pointer
- * Return: the buffer index
- */
-int print_perc(char *buff_dest, va_list arg, int buff_count)
-{
-	char c = va_arg(arg, int);
-
-	/* reassigned due to unexpected behaviour */
-	c = '%';
-
-	buff_dest[buff_count] = c;
-
-	return (++buff_count);
-}
->>>>>>> 376bba663e0907b6a8e7886299124b0a9a1da23e
